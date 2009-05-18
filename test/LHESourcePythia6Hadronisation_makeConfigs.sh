dd399380f50875831db21cb14080b4660f5f18ef
saout <sha1-a9a1d87b03630df246ec7c3919d4d79e9a149492@cern.ch>
2009-05-18 15:36:12 +0000

    Fix MCDBSource  with skipEvents, remove Py6 and Hw6, since they have now hadronizer filters in the new framework and clean up examples.

--- test/LHESourcePythia6Hadronisation_makeConfigs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cmsDriver.py GeneratorInterface/LHEInterface/LHESourcePythia6Hadronisation_cff.py -s GEN:ProducerFilterSequence --eventcontent RAWSIM --datatier GEN --conditions FrontierConditions_GlobalTag,IDEAL_V9::All -n 100 --no_exec
-cmsDriver.py GeneratorInterface/LHEInterface/LHESourcePythia6Hadronisation_cff.py -s GEN:ProducerFilterSequence,SIM,DIGI,L1,DIGI2RAW,HLT --eventcontent RAWSIM --datatier GEN-SIM-RAW --conditions FrontierConditions_GlobalTag,IDEAL_V9::All -n 100 --no_exec
